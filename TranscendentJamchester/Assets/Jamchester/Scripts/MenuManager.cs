67767807a85b39024b06147c1b6551565006d94d
Josh Hale <josh_8d@hotmail.co.uk>
2017-06-25 04:06:13 +0100

    Added New Assets

---
+++ TranscendentJamchester/Assets/Jamchester/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject helpCanvas;
+	// Use this for initialization
+	
+	public void ShowCanvas()
+	{
+		helpCanvas.SetActive(true);
+	}
+
+	public void HideCanvas()
+	{
+		helpCanvas.SetActive(false);
+	}
+}
